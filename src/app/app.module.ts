6b9b121d5fd2fe040f6c20daaf6f1785a82985c2
Luke Cureton <professionalluke30@gmail.com>
2018-11-29 16:19:36 -0500

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavComponent,
-    AboutComponent,
-    ContactComponent
   ],
   imports: [
     BrowserModule,
